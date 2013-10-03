0b8ff16ea153a9c84eac6a451d6ad3dee774f3c1
maedeh kafiyan <maedeh.kafiyan@gmail.com>
2013-10-02 20:26:12 -0700

    login page wiht out php

---
+++ login/adminlogin.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+	<title>
+	<meta charset="UTF-8" />
+	<title>KasraSweets</title>
+	<link rel="stylesheet" type="text/css" href="1styles.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	</title>
+</head>
+<body>
+<div class="container">
+	<section class="adminlogin">
+		<div class="chef"></div>
+		<div class="br">
+			<form action="adminlogin.php" method="post">
+				<input name="username" type="text" placeholder="username" /> <br />
+				<input name="password" type="password"  placeholder="password" /> <br />
+				<button type="submit">login</button>
+			</form>	
+		</div>
+	</section>
+</div>	
+</body>
+</html>
