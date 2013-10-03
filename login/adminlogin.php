3e07f7a1a91f50c1c887dab53dd9b931fec8445a
maedeh kafiyan <maedeh.kafiyan@gmail.com>
2013-10-03 09:40:09 -0700

    fixing php login admin page bug

--- login/adminlogin.php
+++ login/adminlogin.php
@@ -1,37 +1,17 @@
-<!doctype html>
-<html>
-<head>
-	<title>
-	<meta charset="UTF-8" />
-	<title>KasraSweets</title>
-	<link rel="stylesheet" type="text/css" href="1styles.css">
-	<link rel="stylesheet" type="text/css" href="style.css">
-	</title>
-</head>
-<body>
-<div class="container">
-	<section class="adminlogin">
-		<div class="chef"></div>
-		<div class="br">
-			<form action="adminlogin.php" method="post">
-				<input name="username" type="text" placeholder="username" /> <br />
-				<input name="password" type="password"  placeholder="password" /> <br />
-				<button type="submit">login</button>
-			</form>	
-		</div>
-	</section>
-</div>
+
 <?php 
 $user=$_POST['username'];
 $password=$_POST['password'];
-if($username==KasraSweet && $password==246810){
  mysql_connect("localhost","root","");
  mysql_select_db("kasraproject");
- mysql_query("select username , pass from adminlogin");
-	 if (username==$user && pass==$password) {
-		echo "har har har !!!";
-	 }
-}
+ $sq=mysql_query("select * from adminlogin where username='$user' and pass='$password'");
+	if($row=mysql_fetch_assoc($sq)){
+		?>
+		
+		<? php
+	}
+	else{
+			header("location: loginadmin.html");
+		}
 ?>	
-</body>
-</html>
+
