43c52ff9aff650861c19398e41dcd1023a40908d
mo-dashti1 <mo_dashti1@yahoo.com>
2013-10-03 21:20:07 +0330

    Update adminlogin.php
    :)

--- login/adminlogin.php
+++ login/adminlogin.php
@@ -1,17 +1,36 @@
-
 <?php 
+// لازمه که حتما این خط رو بالای صفحه بنویسی یا اگه بالای صفحه نیست، قبلش نباید هیج کارکتری رو صفحه چاپ بشه. حتی یک اسپیش
+session_start();
 $user=$_POST['username'];
 $password=$_POST['password'];
- mysql_connect("localhost","root","");
- mysql_select_db("kasraproject");
- $sq=mysql_query("select * from adminlogin where username='$user' and pass='$password'");
+
+if(!isset($_SESSION['username']))
+{
+	// if user has not loged in yet
+	 mysql_connect("localhost","root","");
+	 mysql_select_db("kasraproject");
+ 
+	 // it's better to use hashed password instead of simple one
+	 
+	 $sq=mysql_query("select * from adminlogin where username='$user' and pass='$password'");
 	if($row=mysql_fetch_assoc($sq)){
-		?>
 		
-		<? php
+		// you can add any index to $SESSION variable.  
+		$_SESSION['username'] = $row['username'];
+		$_SESSION['name'] = $row['name'];
+		$_SESSION['level'] = $row['level'];
 	}
 	else{
 			header("location: loginadmin.html");
 		}
+}
+else
+{
+	// user's loged in already. rediret uesr to you admin page.
+}
+
+
+// to log out, you have to unset session indexes
+// unset($_SESSION['username']);
 ?>	
 
